c41e3c6d8510cd42d40df47bd6a7d9cc2c83a9ed
npes <npesdk@gmail.com>
2017-09-27 15:55:02 +0200

    Initial commit

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace Arrays
 {
@@ -10,6 +10,23 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            string[] fruits = { "apple", "banana", "orange" };
+            Console.WriteLine("Fruits array has length of: " + fruits.Length);
+
+
+            Console.WriteLine("Fruits array contains");
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+
+            foreach (string fruit in fruits) // foreach is only for strings
+            {
+                Console.WriteLine(fruit);
+                Console.WriteLine(fruit.GetType()); //the type of element in array
+            }
+
+            Console.ReadLine();
         }
     }
 }
